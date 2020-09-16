000a85f9d2e6659ddd9c4ccde4cb8f0925c03486
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-09-16 15:41:58 +0100

    Added OPCube interface

---
+++ src/scs_psu/psu/opcube_v1/psu_opcube_v1.py
@@ -0,0 +1,134 @@
+"""
+Created on 1 Apr 2020
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+Lightweight system Raspberry Pi Zero header + mobile power pack + fuel gauge
+"""
+
+from scs_core.psu.psu import PSU
+
+from scs_host.bus.i2c import I2C
+from scs_host.sys.host import Host
+
+from scs_psu.psu.mobile_v2.psu_status import PSUStatus, ChargeStatus
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class PSUOPCubeV1(PSU):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def name(cls):
+        return 'OPCubeV1'
+
+
+    @classmethod
+    def requires_interface(cls):
+        return True
+
+
+    @classmethod
+    def uses_batt_pack(cls):
+        return True
+
+
+    @classmethod
+    def report_class(cls):
+        return PSUStatus
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, header, batt_pack):
+        """
+        Constructor
+        """
+        self.__header = header                                      # PZHB
+        self.__batt_pack = batt_pack                                # BattPackV1
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def open(self):
+        I2C.open(Host.I2C_SENSORS)
+
+        self.__header.button_enable()
+
+
+    def close(self):
+        I2C.close()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def status(self):
+        standby = self.__header.button_pressed()
+        power_in = self.__header.read_batt_v()
+
+        try:
+            batt_status = self.batt_pack.sample()
+            input_power_present = None if batt_status is None else batt_status.input_power_present
+            charge_status = ChargeStatus.construct_from_batt_status(batt_status)
+
+        except (AttributeError, OSError):
+            input_power_present = None
+            charge_status = None
+
+        return PSUStatus(standby, input_power_present, power_in, charge_status)
+
+
+    def charge_min(self):
+        return None if self.batt_pack is None else self.__batt_pack.charge_min()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def version(self):
+        return self.__header.version_ident()
+
+
+    def uptime(self):
+        return None
+
+
+    def host_shutdown_initiated(self):
+        return self.__header.host_shutdown_initiated()
+
+
+    def watchdog_start(self, interval):
+        return None
+
+
+    def watchdog_stop(self):
+        return None
+
+
+    def watchdog_touch(self):
+        return None
+
+
+    def charge_pause(self, on):
+        return None
+
+
+    def charge_dead(self, on):
+        return None
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def batt_pack(self):
+        return self.__batt_pack
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "PSUOPCubeV1:{header:%s, batt_pack:%s}" % (self.__header, self.batt_pack)
---
+++ src/scs_psu/psu/opcube_v1/psu_status.py
@@ -0,0 +1,186 @@
+"""
+Created on 1 Apr 2020
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from collections import OrderedDict
+
+from scs_core.data.datetime import Timedelta
+from scs_core.data.datum import Datum
+from scs_core.data.json import JSONable
+
+from scs_core.psu.psu_report import PSUReport
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class PSUStatus(PSUReport):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        standby = jdict.get('standby')
+        input_power_present = jdict.get('in')
+        v_in = jdict.get('pwr-in')
+        charge_status = ChargeStatus.construct_from_jdict(jdict.get('batt'))
+
+        return PSUStatus(standby, input_power_present, v_in, charge_status)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, standby, input_power_present, v_in, charge_status):
+        """
+        Constructor
+        """
+        self.__standby = standby                                    # bool
+        self.__input_power_present = input_power_present            # bool
+        self.__v_in = Datum.float(v_in, 1)                          # PSU input voltage  float
+        self.__charge_status = charge_status                        # ChargeStatus
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['standby'] = self.standby
+        jdict['in'] = self.input_power_present
+        jdict['pwr-in'] = self.v_in
+        jdict['batt'] = None if self.charge_status is None else self.charge_status.as_json()
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def below_power_threshold(self, charge_min):
+        if self.charge_status is None:
+            return False                                    # power threshold cannot be identified
+
+        if self.charge_status.tte is None:
+            return False                                    # device is not running on battery or gauge disconnected
+
+        return self.charge_status.charge < charge_min
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # PSUReport properties...
+
+    @property
+    def standby(self):
+        return self.__standby
+
+
+    @property
+    def input_power_present(self):
+        return self.__input_power_present
+
+
+    @property
+    def v_in(self):
+        return self.__v_in
+
+
+    @property
+    def batt_percent(self):
+        return None if self.__charge_status is None else self.__charge_status.charge
+
+
+    @property
+    def charge_status(self):
+        return self.__charge_status
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "PSUStatus:{standby:%s, input_power_present:%s, v_in:%s, charge_status:%s}" % \
+               (self.standby, self.input_power_present, self.v_in, self.charge_status)
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class ChargeStatus(JSONable):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @classmethod
+    def construct_from_jdict(cls, jdict):
+        if not jdict:
+            return None
+
+        charge = jdict.get('chg')
+        tte = Timedelta.construct_from_jdict(jdict.get('tte'))
+        ttf = Timedelta.construct_from_jdict(jdict.get('ttf'))
+
+        return cls(charge, tte, ttf)
+
+
+    @classmethod
+    def construct_from_batt_status(cls, batt_status):
+        if batt_status is None:
+            return None
+
+        charge = int(round(batt_status.charge.percent))
+        tte = batt_status.tte
+        ttf = batt_status.ttf
+
+        return cls(charge, tte, ttf)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, charge, tte, ttf):
+        """
+        Constructor
+        """
+        self.__charge = Datum.int(charge)                       # int           percentage
+        self.__tte = tte                                        # TimeDelta     time to empty
+        self.__ttf = ttf                                        # TimeDelta     time to full
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def as_json(self):
+        jdict = OrderedDict()
+
+        jdict['chg'] = self.charge
+        jdict['tte'] = None if self.tte is None else self.tte.as_json()
+        jdict['ttf'] = None if self.ttf is None else self.ttf.as_json()
+
+        return jdict
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def charge(self):
+        return self.__charge
+
+
+    @property
+    def tte(self):
+        return self.__tte
+
+
+    @property
+    def ttf(self):
+        return self.__ttf
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "ChargeStatus:{charge:%s, tte:%s, ttf:%s}" %  (self.charge, self.tte, self.ttf)
