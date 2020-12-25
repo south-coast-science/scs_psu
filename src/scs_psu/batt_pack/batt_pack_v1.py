c1140eb050d7a1332ada01228f7e1b97b9237dd9
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-12-25 13:19:35 +0000

    Refactored BattPack classes

--- src/scs_psu/batt_pack/batt_pack_v1.py
+++ src/scs_psu/batt_pack/batt_pack_v1.py
@@ -3,20 +3,23 @@ Created on 3 Oct 2019
 
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 
+Used by version 1 handhelds.
+
 Tuned for 2 x Nitecore IMR 18650 (3.7 V, 3100 mAh)
 https://www.nitecore.co.uk/Shop/Products/Batteries/13663-Nitecore-IMR-18650-Battery-for-TM28-Torch.html#Features
 """
 
 import json
 
-from scs_psu.batt_pack.fuel_gauge.max17055.max17055 import MAX17055
+from scs_psu.batt_pack.batt_pack import BattPack
+
 from scs_psu.batt_pack.fuel_gauge.max17055.max17055_config import MAX17055Config
 from scs_psu.batt_pack.fuel_gauge.max17055.max17055_params import MAX17055Params
 
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class BattPackV1(object):
+class BattPackV1(BattPack):
     """
     classdocs
     """
@@ -33,14 +36,6 @@ class BattPackV1(object):
     def name():
         return 'PackV1'
 
-    # designCap = 0x1450
-    # ichgterm = 0x333
-    # modelcfg = 0x8000
-    # QRTable00 = 0x1050
-    # QRTable10 = 0x2012
-    # VEmpty = 0xa561
-    # RCOMP0 = 0x004d
-    # TempCo = 0x223e
 
     @staticmethod
     def gauge_conf():
@@ -64,21 +59,8 @@ class BattPackV1(object):
 
 
     @classmethod
-    def param_save_interval(cls):
-        return MAX17055.param_save_interval()
-
-
-    @classmethod
     def default_params(cls):
         return MAX17055Params.construct_from_jdict(json.loads(cls.__DEFAULT_PARAMS))
-
-
-    @classmethod
-    def construct(cls):
-        conf = cls.gauge_conf()
-        gauge = MAX17055(conf)
-
-        return BattPackV1(gauge)
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -87,65 +69,4 @@ class BattPackV1(object):
         """
         Constructor
         """
-        self.__gauge = gauge
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def initialise(self, host, force_config=False):
-        try:
-            if not self.__gauge.read_power_on_reset() and not force_config:
-                return None
-
-            params = MAX17055Params.load(host)
-
-            if params is None:
-                params = self.default_params()
-
-            self.__gauge.initialise(force_config=force_config)
-            self.__gauge.write_params(params)
-            self.__gauge.clear_power_on_reset()
-
-            return params
-
-        except OSError:
-            return None
-
-
-    def read_learned_params(self):
-        try:
-            return self.__gauge.read_learned_params()
-        except OSError:
-            return None
-
-
-    def write_params(self, params: MAX17055Params):
-        try:
-            self.__gauge.write_params(params)
-            return True
-
-        except OSError:
-            return False
-
-
-    def sample(self):
-        try:
-            return self.__gauge.sample()
-        except OSError:
-            return None
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    @property
-    def cycles(self):
-        try:
-            return self.__gauge.read_learned_params().cycles
-        except OSError:
-            return None
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def __str__(self, *args, **kwargs):
-        return "BattPackV1:{gauge:%s}" %  self.__gauge
+        super().__init__(gauge)
