42f2dac39546d34cf7996660ff2ab6187d221433
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-02 12:54:19 +0000

    Updated MAX17055Params handling

--- src/scs_psu/batt_pack/fuel_gauge/max17055/max17055_params.py
+++ src/scs_psu/batt_pack/fuel_gauge/max17055/max17055_params.py
@@ -14,9 +14,9 @@ Document example:
 
 from collections import OrderedDict
 
+from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.datum import Datum
 from scs_core.data.json import PersistentJSONable
-from scs_core.data.datetime import LocalizedDatetime
 
 
 # --------------------------------------------------------------------------------------------------------------------
