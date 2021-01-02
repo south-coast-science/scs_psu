49cbd4bbf55ef923ec1959b5a94fe1dbfba5bc03
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-01-02 10:47:23 +0000

    Upgraded fuel_gauge_calib.py

--- tests/batt_pack/fuel_guage/max17055/max17055_params_test.py
+++ tests/batt_pack/fuel_guage/max17055/max17055_params_test.py
@@ -8,6 +8,7 @@ Created on 3 Oct 2016
 https://github.com/electricimp/MAX17055
 """
 
+from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.json import JSONify
 
 from scs_psu.batt_pack.fuel_gauge.max17055.max17055_params import MAX17055Params
@@ -21,7 +22,7 @@ full_cap_rep = 3
 full_cap_nom = 4
 cycles = 5
 
-params = MAX17055Params(r_comp_0, temp_co, full_cap_rep, full_cap_nom, cycles)
+params = MAX17055Params(LocalizedDatetime.now(), r_comp_0, temp_co, full_cap_rep, full_cap_nom, cycles)
 print(params)
 print("-")
 
