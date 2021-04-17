a01a1ac7bee58b3f167f9713a0b4d24cb4439e0c
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-04-17 11:58:09 +0100

    Upgraded fuel_gauge_calib.py

--- src/scs_psu/batt_pack/batt_pack_v2.py
+++ src/scs_psu/batt_pack/batt_pack_v2.py
@@ -25,11 +25,8 @@ class BattPackV2(BattPack):
 
     __CHARGE_MINIMUM =        1         # percent
 
-    __DEFAULT_PARAMS =  '{"calibrated-on": "2021-04-17T07:19:56Z", "r-comp-0": 19, "temp-co": 8766, ' \
-                        '"full-cap-rep": 3643, "full-cap-nom": 5429, "cycles": 49}'
-
-    # __DEFAULT_PARAMS =  '{"calibrated-on": "2021-01-03T09:25:52Z", "r-comp-0": 255, "temp-co": 9278, ' \
-    #                     '"full-cap-rep": 4173, "full-cap-nom": 1927, "cycles": 150}'
+    __DEFAULT_PARAMS =  '{"calibrated-on": "2021-04-17T10:51:25Z", "r-comp-0": 263, "temp-co": 8766, ' \
+                        '"full-cap-rep": 3643, "full-cap-nom": 5429, "cycles": 98}'
 
 
     # ----------------------------------------------------------------------------------------------------------------
@@ -44,7 +41,7 @@ class BattPackV2(BattPack):
         des_cap = 3000                  # mAh was 1200
         sense_res = 0.01                # Ω
         chrg_term = 10                  # mA
-        empty_v_target = 3.3            # V
+        empty_v_target = 2.7            # V
         recovery_v = 3.8                # V
 
         chrg_v = Max17055Config.CHRG_V_4_2
