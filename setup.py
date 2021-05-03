d309fe8c2c050b7e449774eb82e7d3fc4b2cdbf7
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-05-03 12:46:10 +0100

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
+"""
+Created on 4 Sep 2020
+Updated 23 Mar 2021
+
+@author: Jade Page (jade.page@southcoastscience.com)
+
+https://packaging.python.org/tutorials/packaging-projects/
+https://packaging.python.org/guides/single-sourcing-package-version/
+"""
+
+import codecs
+import os
+
 from setuptools import setup, find_packages
 
+
+# --------------------------------------------------------------------------------------------------------------------
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            return line.split("'")[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+# --------------------------------------------------------------------------------------------------------------------
 
 with open('requirements.txt') as req_txt:
     required = [line for line in req_txt.read().splitlines() if line]
 
 setup(
     name='scs_psu',
-    version='0.1.0',
+    version=get_version("scs_psu/__init__.py"),
     description='Communication and control of the South Coast Science power supply boards for BeagleBone.',
     author='South Coast Science',
     author_email='contact@southcoastscience.com',
