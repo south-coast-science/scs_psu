67c6cb9fd947ae96c774f0e9e024149ef6862702
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-07-11 09:12:58 +0100

    Cleaning

--- src/scs_psu/batt_pack/fuel_gauge/max17055/max17055_params.py
+++ src/scs_psu/batt_pack/fuel_gauge/max17055/max17055_params.py
@@ -36,7 +36,7 @@ class Max17055Params(PersistentJSONable):
     # ----------------------------------------------------------------------------------------------------------------
 
     @classmethod
-    def construct_from_jdict(cls, jdict, shell=False):
+    def construct_from_jdict(cls, jdict, skeleton=False):
         if not jdict:
             return None
 
