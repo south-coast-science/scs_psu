7dbbda696b093c7be39ed2224388dd7755ce4d1c
Bruno Beloff <bruno.beloff@southcoastscience.com>
2024-04-25 11:32:20 +0100

    Upgraded JSONify

--- src/scs_psu/psu/mobile_v1/psu_status.py
+++ src/scs_psu/psu/mobile_v1/psu_status.py
@@ -44,7 +44,7 @@ class PSUStatus(PSUReport):
 
     # ----------------------------------------------------------------------------------------------------------------
 
-    def as_json(self):
+    def as_json(self, **kwargs):
         jdict = OrderedDict()
 
         jdict['src'] = self.source
